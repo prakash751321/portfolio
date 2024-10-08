8a4f1043344d78ada17a3d3d751a0b4aa94212c3
Jyoti Prakash Nayak <82704014+prakash751321@users.noreply.github.com>
2024-10-08 18:56:02 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 function Home() {
 
+  const handleDownload = () => {
+    // This will trigger the download of the CV file
+    const link = document.createElement('a');
+    link.href = './Jyoti_Prakash_Resume.pdf'; // Update the path if your CV is in a different location
+    link.download = 'CV.pdf'; // The name that will be used for the downloaded file
+    link.click();
+};
+
   return (
     <div id='home'>
       {/* <p className='text-yellow-300 text-3xl mt-8'>Hii i am <br /> <span className='text-6xl text-white ml-4 font-["Qwitcher_Grypen"]'>Jyoti Prakash Nayak</span></p>
@@ -12,9 +20,12 @@ function Home() {
           <h1 className="text-4xl font-extrabold text-gray-900">Hi, I'm Jyoti Prakash Nayak</h1>
           <p className="mt-4 text-xl text-gray-500">A Frontend Developer passionate about creating responsive and interactive web and mobile experiences.</p>
           <div className="mt-8 flex justify-center">
-            <a href="#contact" className="bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700">
-              Get in Touch
-            </a>
+            <button
+                onClick={handleDownload}
+                className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
+            >
+                Download CV
+            </button>
           </div>
         </div>
       </header>
